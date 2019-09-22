f2696eef6fea6ebdacee912f441227a3df277fc8
Roman Niukhalov <r.nyukhalov@gmail.com>
2019-09-22 16:40:05 +0800

    Enable showing hidden files and use vim keys during autocomplition

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -4,3 +4,14 @@ fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 # completion
 autoload -U compinit
 compinit
+
+# include hidden files
+setopt globdots
+
+# use the vi navigation keys in menu completion
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
